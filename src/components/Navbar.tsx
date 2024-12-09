4d44ac4716a4d6d01b9975c396281474d3337300
huda-7077 <85928499+huda-7077@users.noreply.github.com>
2024-12-09 14:22:44 +0700

    feat: keeplogin

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,17 @@
+"use client";
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { logoutAction } from "@/redux/slices/userSlice";
 import Link from "next/link";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+  const user = useAppSelector((state) => state.user);
+
+  const logout = () => {
+    localStorage.removeItem("blog-storage");
+    dispatch(logoutAction());
+  };
+
   return (
     <nav className="bg-slate-400">
       <div className="container mx-auto px-4">
@@ -8,9 +19,15 @@ const Navbar = () => {
           <Link href="/" className="text-xl font-bold">
             Logo
           </Link>
-          <div className="flex items-center gap-8 font-medium">
+
+          <div className="flex cursor-pointer items-center gap-8 font-medium">
             <Link href="/">Home</Link>
             <Link href="/">Profile</Link>
+            {user.id ? (
+              <p onClick={logout}>Sign out</p>
+            ) : (
+              <Link href="/login">Sign in</Link>
+            )}
           </div>
         </div>
       </div>
--- src/hooks/api/auth/useLogin.ts
+++ src/hooks/api/auth/useLogin.ts
@@ -1,8 +1,11 @@
 "use client";
 
 import { axiosInstance } from "@/lib/axios";
+import { useAppDispatch } from "@/redux/hooks";
+import { loginAction } from "@/redux/slices/userSlice";
 import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 
 interface LoginPayload {
@@ -11,13 +14,18 @@ interface LoginPayload {
 }
 
 const useLogin = () => {
+  const router = useRouter();
+  const dispatch = useAppDispatch();
   return useMutation({
     mutationFn: async (payload: LoginPayload) => {
       const { data } = await axiosInstance.post("/auth/Login", payload);
       return data;
     },
-    onSuccess: () => {
+    onSuccess: (data) => {
       toast.success("Login Success");
+      dispatch(loginAction(data)); // masukkin data ke global state
+      localStorage.setItem("blog-storage", JSON.stringify(data)); // masukkin data ke local storage
+      router.replace("/");
     },
     onError: (error: AxiosError<any>) => {
       toast.error(error.response?.data);
