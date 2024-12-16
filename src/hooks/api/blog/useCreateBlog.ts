b3952c294e4b3436d9f2f9bf687fa06bdc8240c4
Muhammad Masyhuda <85928499+huda-7077@users.noreply.github.com>
2024-12-16 09:43:45 +0700

    feat: create blog (#8)

---
+++ src/hooks/api/blog/useCreateBlog.ts
@@ -0,0 +1,46 @@
+"use client";
+
+import useAxios from "@/hooks/useAxios";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+
+interface CreateBlogPayload {
+  title: string;
+  category: string;
+  description: string;
+  content: string;
+  thumbnail: File | null;
+}
+
+const useCreateBlog = () => {
+  const router = useRouter();
+  const { axiosInstance } = useAxios();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (payload: CreateBlogPayload) => {
+      const createBlogForm = new FormData();
+
+      createBlogForm.append("title", payload.title);
+      createBlogForm.append("category", payload.category);
+      createBlogForm.append("description", payload.description);
+      createBlogForm.append("content", payload.content);
+      createBlogForm.append("thumbnail", payload.thumbnail!);
+
+      const { data } = await axiosInstance.post("/blogs", createBlogForm);
+      return data;
+    },
+    onSuccess: async () => {
+      toast.success("Create blog success");
+      await queryClient.invalidateQueries({ queryKey: ["blogs"] });
+      router.push("/");
+    },
+    onError: (error: AxiosError<any>) => {
+      toast.error(error.response?.data.message || error.response?.data);
+    },
+  });
+};
+
+export default useCreateBlog;
